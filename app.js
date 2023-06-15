5f8a5a4d1548365e9bda69e569be81e15eda477c
Huranu <hulan0000@gmail.com>
2023-06-15 15:29:34 +0800

    second commit

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Shoonii ali talaar buusniig hadgalah huvisgach 1-6(random)
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // window.document.querySelector("#score-0").textContent = dice;
 
 // document.querySelector("#score-1").innerHTML = "<em>Teneg!<em>";
 
 // programm ehleh
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-console.log("shoo:" + dice);
+var diceDOM = document.querySelector(".dice");
+diceDOM.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + diceNumber + ".png";
+});
